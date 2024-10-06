e5f802049045dcb7ae5f8e82d7dcbe2858976462
supriya kumari <supriyakumarisj@gmail.com>
2024-10-06 17:09:37 +0530

    Added even number calculation  logic

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 
 let num = [4, 99, 56, 78, 89, 23, 66];
 
-let even = [];
-
-for(let i = 0; i < num.length; i++){
-    if(num[i] % 2 === 0){
-        even.push(num[i]);
-    }
-}
-console.log("Even : ", even);
-
 // Odd Number
 
 let odd = [];
@@ -21,6 +12,15 @@ for(let i = 0; i < num.length; i++){
     }
 }
 console.log("Odd : ", odd);
+
+let even = [];
+
+for(let i = 0; i < num.length; i++){
+    if(num[i] % 2 === 0){
+        even.push(num[i]);
+    }
+}
+console.log("Even : ", even);
 
 // n = 4, pattern print
 // *
